b1785046e3669bc07c993d35e78392c83e7e5117
Shisun <worlddominator007@gmail.com>
2021-05-05 17:10:25 +0700

    finishing touch add keyboard support

--- index.js
+++ index.js
@@ -1,73 +1,157 @@
+const allButton = document.querySelectorAll("button");
+const multiplyButton = document.querySelector("#multiply");
 const digits = document.querySelectorAll(".digit__button");
 const ops = document.querySelectorAll(".op__button");
-const reset_button = document.querySelector("#reset");
-const current_result = document.querySelector("#current__result");
-const previous_operation = document.querySelector("#previous__operation");
+const resetButton = document.querySelector("#reset");
+const currentResult = document.querySelector("#current__result");
+const previousButton = document.querySelector("#previous__operation");
+const backButton = document.querySelector("#backspace");
 const dot = document.querySelector("#dot");
 
 let first = "";
 let second = "";
 let operator = "";
+let key = "";
 let resultExist = false;
 
 digits.forEach((digit) => {
   digit.addEventListener("click", (e) => {
-    if (current_result.innerText.includes(".")) {
-      dot.disabled = true;
-    }
-
-    if (current_result.innerText === "0" || second === null) {
-      current_result.innerText = "";
-    }
-
-    if (operator) {
-      if (resultExist) {
-        current_result.innerText = "";
-        resultExist = false;
-      }
-      current_result.innerText += e.target.innerText;
-      second = current_result.innerText;
-    } else {
-      appendToResult(e);
-    }
+    inputNumber(e);
   });
 });
 
 //loop through operator except equal
 for (let i = 0; i < ops.length - 1; i++) {
   ops[i].addEventListener("click", (e) => {
-    if (/[0-9]/.test(current_result.innerText)) {
-      if (second && operator) {
-        const result = execute(first, operator, second);
-        current_result.innerText = Math.round(result() * 100) / 100;
-        resultExist = true;
-        second = "";
-        dot.disabled = false;
-        return (first = current_result.innerText);
-      }
-
-      //update first value and display
-      first = current_result.innerText;
-      operator = e.target.innerText;
-      current_result.innerText = "";
-      
-    }
+    inputOperator(e);
   });
 }
 
 //equal button
 ops[4].addEventListener("click", () => {
   if (first && operator && second) {
+    //update current operation here
+    displayPreviousOperation();
+    //continue
     calculate();
+    clear();
   }
 });
 
-reset_button.onclick = reset;
+dot.onclick = addDot;
+
+resetButton.onclick = reset;
+
+backButton.onclick = backspace;
+
+window.addEventListener("keydown", (e) => {
+  keyboardPressed(e);
+});
+
+function returnKeyPress(e) {
+  return e.key;
+}
+
+function keyboardPressed(e) {
+  key = returnKeyPress(e);
+  allButton.forEach((button) => {
+    if (key === button.innerText) {
+      applyClickAndFocusOnButton(button);
+    }
+  });
+  if (key === "*") {
+    applyClickAndFocusOnButton(multiplyButton);
+  }
+  if (key === "Backspace") {
+    applyClickAndFocusOnButton(backButton);
+  }
+  if (key === "Enter") {
+    applyClickAndFocusOnButton(ops[4]);
+  }
+  if (key === "Escape") {
+    applyClickAndFocusOnButton(resetButton);
+  }
+}
+
+function inputNumber(e) {
+  if (currentResult.innerText === "0" || second === null) {
+    currentResult.innerText = "";
+  }
+
+  if (operator) {
+    if (resultExist) {
+      currentResult.innerText = "";
+      resultExist = false;
+    }
+    currentResult.innerText += e.target.innerText;
+    second = currentResult.innerText;
+  } else {
+    appendToResult(e);
+  }
+}
+
+function inputOperator(e) {
+  if (parseFloat(currentResult.innerText)) {
+    if (second && operator) {
+      calculate();
+      resultExist = true;
+      operator = e.target.innerText;
+      //update previous operation
+      displayPreviousOperation();
+      second = "";
+      return (first = currentResult.innerText);
+    }
+
+    //update first value and display
+    first = currentResult.innerText;
+    operator = e.target.innerText;
+    currentResult.innerText = "";
+  }
+}
+
+function applyClickAndFocusOnButton(button) {
+  button.click();
+  button.focus();
+  setTimeout(() => {
+    button.blur();
+  }, 1000);
+}
 
 function appendToResult(e) {
-  current_result.innerText += e.target.innerText;
-  current_result.scrollLeft +=
-    current_result.offsetWidth + current_result.offsetLeft;
+  currentResult.innerText += e.target.innerText;
+  currentResult.scrollLeft +=
+    currentResult.offsetWidth + currentResult.offsetLeft;
+}
+
+function addDot() {
+  if (currentResult.innerText.includes(".")) return;
+  currentResult.innerText += ".";
+}
+
+function reset() {
+  first = "";
+  second = "";
+  operator = "";
+  previousButton.innerText = 0;
+  resultExist = false;
+  currentResult.innerText = 0;
+}
+
+function clear() {
+  first = "";
+  operator = "";
+  second = "";
+}
+
+function backspace() {
+  if (currentResult.innerText.length <= 0) return;
+  if (currentResult.innerText.length == 1) return (currentResult.innerText = 0);
+  currentResult.innerText = currentResult.innerText.slice(0, -1);
+  second = currentResult.innerText;
+}
+
+function displayPreviousOperation() {
+  previousButton.innerText = `${first} ${operator} ${second}`;
 }
 
 function add(a, b) {
@@ -99,7 +183,7 @@ function execute(first, op, second) {
     case "/":
       if (second === 0) {
         second = null;
-        return (current_result.innerText = "Cannot divide by 0");
+        return (currentResult.innerText = "Cannot divide by 0");
       }
       return new Function(`return ${divide(first, second)}`);
     default:
@@ -107,19 +191,7 @@ function execute(first, op, second) {
   }
 }
 
-function reset() {
-  first = "";
-  second = "";
-  operator = "";
-  current_result.innerText = "0";
-  dot.disabled = false;
-}
-
 function calculate() {
   const result = execute(first, operator, second);
-  current_result.innerText = Math.round(result() * 100) / 100;
-  first = current_result.innerText;
-  operator = "";
-  second = "";
-  dot.disabled = false;
+  currentResult.innerText = Math.round(result() * 1000) / 1000;
 }
